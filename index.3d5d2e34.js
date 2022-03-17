c5930481d4dff8776220a1ac99f912c2c40739e5
kadiri2006 <kadiri2006@gmail.com>
2022-03-17 23:53:48 +0530

    Updates

---
+++ index.3d5d2e34.js
@@ -0,0 +1,2 @@
+let collapsible=document.querySelectorAll(".collapsible--expanded");collapsible.forEach((l=>l.addEventListener("click",(()=>l.classList.toggle("collapsible--expanded")))));
+//# sourceMappingURL=index.3d5d2e34.js.map
